a9f8522bb5ddf4be8d6c01163f9ae2532c1817bf
bikram_ghuku <bikramghuku05@gmail.com>
2021-07-31 15:36:25 +0530

    updating capnp zmq impl

--- message/capnp_impl.cpp
+++ message/capnp_impl.cpp
@@ -1,26 +1,30 @@
 #include<capnp/serialize.h>
 #include<capnp/message.h>
 #include<schemas/message.capnp.h>
-#include"zmq_msg.h"
+#include"send_zmq_msg.h"
 
-void write()
+void write(std::string superkey)
 {
     ::capnp::MallocMessageBuilder msg;
     MsgPacket::Builder msgpack = msg.initRoot<MsgPacket>();
     ::capnp::List<KeyValuePair>::Builder keyvaluepair =msgpack.initJson(2);
 
     KeyValuePair::Builder num_one = keyvaluepair[0];
-    num_one.setSuperKey("SensorEvent");
+    num_one.setSuperKey(superkey);
     num_one.setKey("OP");
     num_one.setValue("Noob");
 
     auto msg_array = capnp::messageToFlatArray(msg);
     send_msg(msg_array);
-    
+}
+
+void read(std::string superkery)
+{
+    ::capnp::MallocMessageBuilder msg;
     
 }
 
 int main()
 {
-    write();
+    write("SensorEvent");
 }
---
+++ message/read_zmq_msg.cpp
@@ -0,0 +1,4 @@
+#include<capnp/serialize.h>
+#include<capnp/message.h>
+#include<schemas/message.capnp.h>
+#include"send_zmq_msg.h"
