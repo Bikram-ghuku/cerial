0eb5f03f1e13d5bb9c05f61ec2e9606848958332
bikram_ghuku <bikramghuku05@gmail.com>
2021-08-03 08:17:18 +0530

    updated setup

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,6 +1,11 @@
 sudo apt-get update && sudo apt-get install -y \
     capnproto \
     libcapnp-dev \
-    libzmqpp-dev 
+    libzmqpp-dev \
+    libzmq3-dev \
+    libjsoncpp-dev\
+    python-dev \
+    python3-pip 
 
-cd message && g++ -Wall -I. -O2 -std=c++17 capnp_impl.cpp zmq_msg.h -lcapnp -lkj -lzmqpp -lzmq -o capnp_impl
+pip install pyzmq
+pip install json
