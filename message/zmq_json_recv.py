da03088c9f82d73ce6b1dc87e6939240f3d12b2f
bikram_ghuku <bikramghuku05@gmail.com>
2021-08-16 19:27:14 +0530

    added multiple channel and debug support

--- message/zmq_json_recv.py
+++ message/zmq_json_recv.py
@@ -8,11 +8,17 @@ client = context.socket(sock_typ)
 client.connect("tcp://127.0.0.1:5556")
 
 class subscriber():
-    def __init__(self, request:str):
+    def __init__(self, request:str, debug_mode:bool=False):
         self.request = request
+        self.debug_mode = debug_mode
 
     def get_data(self):
         client.subscribe("")
         msg= client.recv_json()
-        msg_rtn = msg[self.request]
+        if self.debug_mode:
+            print(msg)
+        try:
+            msg_rtn = msg[self.request]
+        except KeyError:
+            pass
         return msg_rtn
